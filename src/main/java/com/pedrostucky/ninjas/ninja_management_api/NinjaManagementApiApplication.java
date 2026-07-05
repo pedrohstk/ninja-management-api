ee8052f613ccc3f9ec1a6081f442b39d994ab151
Pedro Stucky <pedrostucky404@gmail.com>
2026-07-05 14:19:31 -0300

    first commit

---
+++ src/main/java/com/pedrostucky/ninjas/ninja_management_api/NinjaManagementApiApplication.java
@@ -0,0 +1,13 @@
+package com.pedrostucky.ninjas.ninja_management_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NinjaManagementApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NinjaManagementApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/pedrostucky/ninjas/ninja_management_api/NinjaManagementApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pedrostucky.ninjas.ninja_management_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NinjaManagementApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
