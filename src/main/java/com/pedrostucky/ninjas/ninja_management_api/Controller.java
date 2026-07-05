d0eab235ff4722cfe1a78004dc641fe9e6ccefba
Pedro Stucky <pedrostucky404@gmail.com>
2026-07-05 16:10:56 -0300

    Criado controller

---
+++ src/main/java/com/pedrostucky/ninjas/ninja_management_api/Controller.java
@@ -0,0 +1,15 @@
+package com.pedrostucky.ninjas.ninja_management_api;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping
+public class Controller {
+
+    @GetMapping("/")
+    public String boasVindas() {
+        return "Configuração inicial da primeira rota da API.";
+    }
+}
