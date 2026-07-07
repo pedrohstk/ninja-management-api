bc9dfbe021b2a58729ea8483c3c9c26d5e92d080
Pedro Stucky <pedrostucky404@gmail.com>
2026-07-07 18:39:27 -0300

    feat: adicionado NinjaController com rotas CRUD

--- src/main/java/com/pedrostucky/ninjas/ninja_management_api/Ninjas/controller/NinjaController.java
+++ src/main/java/com/pedrostucky/ninjas/ninja_management_api/Ninjas/controller/NinjaController.java
@@ -1,15 +1,37 @@
 package com.pedrostucky.ninjas.ninja_management_api.Ninjas.controller;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping
 public class NinjaController {
 
-    @GetMapping("/")
-    public String boasVindas() {
-        return "Configuração inicial da primeira rota da API.";
+    // Adicionar Ninja (CREATE)
+    @PostMapping("/api/v1/ninja/create")
+    public String createNinja() {
+        return "Ninja cadastrado com sucesso";
+    }
+    // Procurar Ninja por Id (READ)
+    @GetMapping("/api/v1/ninja/{id}")
+    public String getNinjaById() {
+        return "Ninja por id";
+    }
+
+    // Mostrar todos os Ninjas (READ)
+    @GetMapping("api/v1/ninja/all")
+    public String getAllNinjas() {
+        return "Ninjas";
+    }
+
+    // Alterar dados dos Ninjas (UPDATE)
+    @PutMapping("/api/v1/ninja/update/{id}")
+    public String updateNinja() {
+        return "Alterar ninja";
+    }
+
+    //Deletar Ninja (DELETE)
+    @DeleteMapping("/api/v1/ninja/delete/{id}")
+    public String deleteNinja() {
+        return "Deletar ninja";
     }
 }
