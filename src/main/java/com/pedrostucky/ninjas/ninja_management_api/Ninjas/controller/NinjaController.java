d8e75e0eae48c5e21a1f76d74fbb2a95d40d4631
Pedro Stucky <pedrostucky404@gmail.com>
2026-07-07 19:56:13 -0300

    feat: Adicionado serviço de listagem de ninjas (findAllNinjas), alterado rota do NinjaController e criado método no NinjaService

--- src/main/java/com/pedrostucky/ninjas/ninja_management_api/Ninjas/controller/NinjaController.java
+++ src/main/java/com/pedrostucky/ninjas/ninja_management_api/Ninjas/controller/NinjaController.java
@@ -1,36 +1,46 @@
 package com.pedrostucky.ninjas.ninja_management_api.Ninjas.controller;
 
+import com.pedrostucky.ninjas.ninja_management_api.Ninjas.model.NinjaModel;
+import com.pedrostucky.ninjas.ninja_management_api.Ninjas.service.NinjaService;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
+
 @RestController
-@RequestMapping
+@RequestMapping("/api/v1/ninjas")
 public class NinjaController {
 
+    private final NinjaService ninjaService;
+
+    public NinjaController(NinjaService ninjaService) {
+        this.ninjaService = ninjaService;
+    }
+
     // Adicionar Ninja (CREATE)
-    @PostMapping("/api/v1/ninja/create")
+    @PostMapping("/create")
     public String createNinja() {
         return "Ninja cadastrado com sucesso";
     }
     // Procurar Ninja por Id (READ)
-    @GetMapping("/api/v1/ninja/{id}")
+    @GetMapping("/find/{id}")
     public String getNinjaById() {
         return "Ninja por id";
     }
 
     // Mostrar todos os Ninjas (READ)
-    @GetMapping("api/v1/ninja/all")
-    public String getAllNinjas() {
-        return "Ninjas";
+    @GetMapping("/find/all")
+    public List<NinjaModel> findAllNinjas() {
+        return ninjaService.findAllNinjas();
     }
 
     // Alterar dados dos Ninjas (UPDATE)
-    @PutMapping("/api/v1/ninja/update/{id}")
+    @PutMapping("/update/{id}")
     public String updateNinja() {
         return "Alterar ninja";
     }
 
     //Deletar Ninja (DELETE)
-    @DeleteMapping("/api/v1/ninja/delete/{id}")
+    @DeleteMapping("/delete/{id}")
     public String deleteNinja() {
         return "Deletar ninja";
     }
--- src/main/java/com/pedrostucky/ninjas/ninja_management_api/Ninjas/service/NinjaService.java
+++ src/main/java/com/pedrostucky/ninjas/ninja_management_api/Ninjas/service/NinjaService.java
@@ -1,4 +1,23 @@
 package com.pedrostucky.ninjas.ninja_management_api.Ninjas.service;
 
+import com.pedrostucky.ninjas.ninja_management_api.Ninjas.model.NinjaModel;
+import com.pedrostucky.ninjas.ninja_management_api.Ninjas.repository.NinjaRepository;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
 public class NinjaService {
+    private final NinjaRepository ninjaRepository;
+
+    public NinjaService(NinjaRepository ninjaRepository) {
+        this.ninjaRepository = ninjaRepository;
+    }
+
+    // Listar todos os ninjas
+    public List<NinjaModel> findAllNinjas() {
+        return ninjaRepository.findAll();
+    }
 }
+
