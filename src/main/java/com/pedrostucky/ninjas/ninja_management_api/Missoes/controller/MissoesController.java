d6767f9137838e4736a500367d7f00b2d0f6cb48
Pedro Stucky <pedrostucky404@gmail.com>
2026-07-07 18:55:17 -0300

    feat: adicionado MissoesController com rotas CRUD

--- src/main/java/com/pedrostucky/ninjas/ninja_management_api/Missoes/controller/MissoesController.java
+++ src/main/java/com/pedrostucky/ninjas/ninja_management_api/Missoes/controller/MissoesController.java
@@ -1,4 +1,32 @@
 package com.pedrostucky.ninjas.ninja_management_api.Missoes.controller;
 
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping
 public class MissoesController {
+    @PostMapping("/api/v1/missoes/create")
+    public String createMissao() {
+        return "Criar missão";
+    }
+
+    @GetMapping("/api/v1/missoes/all")
+    public String findAllMissoes() {
+        return "Todas as missões";
+    }
+
+    @GetMapping("/api/v1/missoes/{id}")
+    public String findMissaoById() {
+        return "Missao por ID";
+    }
+
+    @PutMapping("/api/v1/missoes/update/{id}")
+    public String updateMissao() {
+        return "Alterar missao";
+    }
+
+    @DeleteMapping("/api/v1/missoes/delete/{id}")
+    public String deleteMissao() {
+        return "Deletar missao";
+    }
 }
