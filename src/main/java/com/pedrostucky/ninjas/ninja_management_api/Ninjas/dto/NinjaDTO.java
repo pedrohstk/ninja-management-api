c2ef3f2d7141c40f57c64eca5f01d7f28ee1baf5
Pedro Stucky <pedrostucky404@gmail.com>
2026-07-09 22:30:28 -0300

    feat: Criado Mapper e DTO para entidade Ninjas

---
+++ src/main/java/com/pedrostucky/ninjas/ninja_management_api/Ninjas/dto/NinjaDTO.java
@@ -0,0 +1,4 @@
+package com.pedrostucky.ninjas.ninja_management_api.Ninjas.dto;
+
+public class NinjaDTO {
+}
---
+++ src/main/java/com/pedrostucky/ninjas/ninja_management_api/Ninjas/mapper/NinjaMapper.java
@@ -0,0 +1,4 @@
+package com.pedrostucky.ninjas.ninja_management_api.Ninjas.mapper;
+
+public class NinjaMapper {
+}
